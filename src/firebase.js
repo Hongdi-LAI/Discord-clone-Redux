5fddd393f65141ea2a077e7807f42e97339671ce
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-10-09 22:03:58 +0800

    firebase database setup

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKjosFA__rHt_lwS9cCKE8Gm_3SJJvFWw",
+  authDomain: "discord-clone-2b376.firebaseapp.com",
+  databaseURL: "https://discord-clone-2b376.firebaseio.com",
+  projectId: "discord-clone-2b376",
+  storageBucket: "discord-clone-2b376.appspot.com",
+  messagingSenderId: "1018406053626",
+  appId: "1:1018406053626:web:5c595dfa08fbb18b258b43",
+  measurementId: "G-FQ9GKX79MR",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAauthProvider();
+
+export { auth, provider };
+export default db;
