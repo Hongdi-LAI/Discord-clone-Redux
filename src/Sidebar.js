7c9eac94eae73e0b2f66a2cf9217b41bc2dde82d
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-10-09 16:40:05 +0800

    Sidebar Componet Done (Hard-Coded)

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,64 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarChannel from "./SidebarChannel";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
+import { Avatar } from "@material-ui/core";
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>Im the side bar</h1>
+      <div className="sidebar__top">
+        <h3>Discord Chats</h3>
+        <ExpandMoreIcon />
+      </div>
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon />
+            <h4>Add Channels</h4>
+          </div>
+          <AddIcon className="sidebar__addChannel" />
+        </div>
+        <div className="sidebar__channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon
+          className="sidebar__voiceIcon"
+          fontSize="large"
+        />
+        <div className="sidebar__voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
+        </div>
+        <div className="sidebar__voiceIcons">
+          <InfoOutlinedIcon />
+          <CallIcon />
+        </div>
+      </div>
+      <div className="sidebar__profile">
+        <Avatar src="" alt="" />
+        <div className="sidebar__profileInfo">
+          <h3>Hongdi</h3>
+          <p>#Discord_ID</p>
+        </div>
+        <div className="sidebar__profileIcons">
+          <MicIcon />
+          <HeadsetIcon />
+          <SettingsIcon />
+        </div>
+      </div>
     </div>
   );
 }
