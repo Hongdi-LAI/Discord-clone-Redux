b492d15ec817afca3a0cb602c6826e8770f5407f
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-10-11 17:07:09 +0800

    Redux Setup & Login and Logout Functionality Setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import Login from "./Login";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar.js";
+import { selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
 
 function App() {
   useEffect(() => {
     document.title = "Discord - Clone";
   }, []);
+
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        /* user logged in */
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        /* user logged out */
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div className="app">
-      {/* sidebar */}
-      <Sidebar />
-      {/* chat */}
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
